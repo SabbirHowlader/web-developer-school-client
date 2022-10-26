c8f058e9f1155871075afb877401dc4dcb55b9da
SabbirHowlader <sabbirahmedreza56@gmail.com>
2022-10-26 21:16:54 +0600

    added category

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Course from '../../Page/Course/Course';
+import Footer from '../../Shared/Footer/Footer';
+import Header from '../../Shared/Header/Header';
 import LeftSideNave from '../../Shared/LeftSideNav/LeftSideNave';
 
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Container> 
                 <Row>
                     <Col lg="4">
@@ -16,6 +19,7 @@ const Main = () => {
                     </Col>
                 </Row>
             </Container>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Shared/LeftSideNav/LeftSideNave.js
+++ src/Shared/LeftSideNav/LeftSideNave.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNave = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () =>{
+        fetch('http://localhost:5000/card-categories')
+        .then(res => res.json())
+        .then(data => setCategories(data));
+    }, [])
     return (
         <div>
-            <h2>This is nav</h2>
+            <h2>All category: {categories.length}</h2>
+            <div>
+                {
+                    categories.map(category => <p kye={category.id}>
+                        <Link to={`/course/${category.id}`}>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
